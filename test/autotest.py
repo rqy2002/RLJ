71000be8858f97ccee114281a65df703f103dd0a
Margatroid <351652632@qq.com>
2018-02-07 20:14:47 +0800

    fix autotest

--- test/autotest.py
+++ test/autotest.py
@@ -12,12 +12,6 @@ def change_dir(dir, cwd):
 def run_test(case):
     change_dir(case, work_dir)
     ret_code = os.system("py -3 ../../rlj/main.py")
-    if ret_code != 0:
-        clean_screen()
-        print ("====================================")
-        print ("Some thing wrong at test {0}".format(case))
-        print ("====================================")
-        exit(0)
 
 test_cases = os.listdir()
 test_cases.remove("autotest.py")
